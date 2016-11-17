1f824a96b7f712eb1eaf8b4e8c36fa823cc43410
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-11-17 14:36:58 -0200

    Add plugin informations

--- energy-s.php
+++ energy-s.php
@@ -1,2 +1,30 @@
 <?php
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+/*
+Plugin Name: Energy-s
+Plugin URI:  https://github.com/LucianoPC/energy-s
+Description: Ferramenta para companhar a economia realizada no consumo de energia
+Version:     0.1
+Author:      Luciano Prestes
+Author URI:  https://github.com/LucianoPC
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: wporg
+Domain Path: /languages
+*/
+
+/*
+Energy-s is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Energy-s is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Energy-s. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
